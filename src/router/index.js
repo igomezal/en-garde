8efeaacb9cdaa22c7474256e30d686509d763e28
Iván Gómez Alonso <yvangomezalonso@gmail.com>
2020-08-04 23:44:34 +0200

    feat: implement basic authentication

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Dashboard from '../views/Dashboard.vue';
+import AuthenticationPage from '../views/AuthenticationPage'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Login',
+    component: AuthenticationPage,
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard
   },
   {
     path: '/about',
---
+++ src/views/AuthenticationPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-btn @click="onGoogleAuthentication" v-if="user === null">
+            <v-icon left>{{ mdiGoogle }}</v-icon> Sign in with Google
+        </v-btn>
+        <p v-else-if="user">You are authenticated!</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  import { mdiGoogle } from '@mdi/js';
+  export default {
+    name: 'AuthenticationPage',
+    data: () => ({
+        mdiGoogle,
+        user: undefined,
+    }),
+    beforeCreate: function() {
+      window.firebase.auth().onAuthStateChanged((user) => {
+        if(user) {
+          this.user = user;
+        } else {
+          this.user = null;
+        }
+      });
+    },
+    methods: {
+      onGoogleAuthentication: function() {
+          const provider = new window.firebase.auth.GoogleAuthProvider();
+          window.firebase.auth().signInWithRedirect(provider);
+          
+      }
+    }
+  }
+</script>
