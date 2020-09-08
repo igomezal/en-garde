68bf87136d0a3807e81b178b6142dadace477714
Iván Gómez Alonso <yvangomezalonso@gmail.com>
2020-09-08 23:25:53 +0200

    feat: change default pwa configuration

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,17 @@
+const appTitle = 'En Garde';
+
 module.exports = {
   pluginOptions: {
     webpackBundleAnalyzer: {
       openAnalyzer: false,
     },
   },
-  transpileDependencies: [
-    'vuetify',
-  ],
-  chainWebpack: config => {
-    config
-      .plugin('html')
-      .tap(args => {
-        args[0].title = 'En Garde';
-        return args;
-      });
+  transpileDependencies: ['vuetify'],
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = appTitle;
+      return args;
+    });
     config.plugin('preload').tap(() => {
       return [
         {
@@ -25,4 +23,11 @@ module.exports = {
       ];
     });
   },
-}
+  pwa: {
+    name: appTitle,
+    themeColor: '#ffffff',
+    msTileColor: '#ffffff',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+  },
+};
